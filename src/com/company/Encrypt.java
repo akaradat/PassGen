cb120d5581770ceb9ebd50d7e3db73e843f4975d
Array <Akrd.Kmnc@kkumail.com>
2017-07-27 02:39:55 +0700

    Create class encrypt

---
+++ src/com/company/Encrypt.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Encrypt {
+    private String code;
+    private String pass;
+
+    public Encrypt(String code) {
+        this.code = code;
+    }
+
+    public String GenPass(){
+        return pass;
+    }
+}
